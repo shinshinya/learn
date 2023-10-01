beeaa87879c1ef39df4e0173f9545490aa5bd1f0
shinya <shinshinya4108@gmail.com>
2023-10-01 18:54:15 +0900

    clean: ディレクトリ構成

--- react/native-js-todo/src/index.js
+++ react/native-js-todo/src/index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 // 静的ファイルを提供するためのミドルウェアを設定する
 // 指定されたディレクトリ内の静的ファイルを提供する
-app.use(express.static("src"));
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
